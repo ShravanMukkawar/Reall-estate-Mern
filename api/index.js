3bc1e65823f0a33cc6a5813719019d610af51830
ShravanMukkawar <mukkawarshravan04@gmail.com>
2024-09-14 23:30:16 +0530

    api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 const app = express();
 
@@ -16,4 +17,7 @@ mongoose
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
-  
+
+app.use(express.json());
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
